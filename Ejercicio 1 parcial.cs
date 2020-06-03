e56fa678e4785c7a96693f8037a8e98ede296590
Darisleydi21 <65923702+Darisleydi21@users.noreply.github.com>
2020-06-02 21:45:51 -0400

    Add files via upload

---
+++ Ejercicio 1 parcial.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string dia;
+            Console.WriteLine("Indica el dia y recuerda usar mayusculas al principio: ");
+            dia = Console.ReadLine();
+            
+            switch(dia)
+            {
+                case "Lunes": Console.WriteLine("No es fin de semana"); break;
+                case "Martes": Console.WriteLine("No es fin de semana"); break;
+                case "Miercoles": Console.WriteLine("No es fin de semana"); break;
+                case "Jueves": Console.WriteLine("No es fin de semana"); break;
+                case "Viernes": Console.WriteLine("No es un fin de semana, pero se acerca el finde"); break;
+                case "Sabado": Console.WriteLine("Es fin de semana"); break;
+                case "Domingo": Console.WriteLine("Es un fin de semana, pero mañana se trabaja recuerda!"); break;
+                default: Console.WriteLine("Error, no se reconoce como dia de la semana, intente de nuevo"); break;
+
+;
+            }
+              
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio 2 parcial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Ingrese precio del producto: ");
+            double precio = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Coloque 1 si pagara con tarjeta, y 2 si pagara en efectivo: ");
+            int tipo = Convert.ToInt32(Console.ReadLine());
+
+            if (tipo == 2)
+            {
+                Console.WriteLine("Su pago ha sido realizado");
+
+            }
+            else
+                Console.WriteLine("Inserte numero de tarjeta: ");
+            int tarjeta = Convert.ToInt32(Console.ReadLine());
+
+            int dig = tarjeta.ToString().Length;
+
+            if (dig == 12)
+            {
+                Console.WriteLine("Su pago se ha realizado con exito: " + precio);
+
+            }
+            else
+                Console.WriteLine("Error, la cantidad de digitos debe ser 12");
+
+            Console.ReadKey();
+            
+        }
+    }
+}
